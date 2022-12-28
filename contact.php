45984125209a7fa12787a0c307719cd5f9a43469
Jubeerathan <jubeerathan@gmail.com>
2022-12-28 22:31:11 +0530

    First

---
+++ contact.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+    <p>
+       this is contact page
+       this is contact page
+       this is contact page
+       this is contact page
+       this is contact page
+       this is contact page
+    </p>
+</body>
+</html>
---
+++ forgotpassword.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>index</title>
+</head>
+<body>
+    <p>
+        forgot password
+        forgot password
+        forgot password
+        forgot password
+        forgot password
+        forgot password
+        forgot password
+        forgot password
+    </p>
+</body>
+</html>
